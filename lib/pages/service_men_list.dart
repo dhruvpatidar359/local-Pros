8d245a9f9adf1e4227823584ed8a8a435265a488
dhruvpatidar359 <dhruvpatidar35@gmail.com>
2023-08-27 20:32:03 +0530

    added nav

---
+++ lib/pages/service_men_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ServiceMenList extends StatefulWidget {
+  const ServiceMenList({super.key});
+
+  @override
+  State<ServiceMenList> createState() => _ServiceMenListState();
+}
+
+class _ServiceMenListState extends State<ServiceMenList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
